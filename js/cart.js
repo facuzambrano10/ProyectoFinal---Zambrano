65b9b056778942b22d83bfcd9ee34c66d24a75c1
Facundo <zamfacu@gmail.com>
2023-08-17 18:49:01 -0300

    reentrega

--- js/cart.js
+++ js/cart.js
@@ -3,8 +3,9 @@ const cantidadElement = document.getElementById("cantidad");
 const precioElement = document.getElementById("precio");
 const carritoVacioElement = document.getElementById("carrito-vacio");
 const totalesContainer = document.getElementById("totales");
+const nuevoBotonAlerta = document.getElementById("nuevo-boton-alerta");
 
-/** Crea las tarjetas de productos teniendo en cuenta lo guardado en localstorage */
+
 function crearTarjetasProductosCarrito() {
   contenedorTarjetas.innerHTML = "";
   const productos = JSON.parse(localStorage.getItem("remeras"));
@@ -66,15 +67,64 @@ function actualizarTotales() {
   }
 }
 
+
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error al obtener los datos de la API o servidor.');
+    }
+    return response.json(); // Parsear la respuesta como JSON
+  })
+  .then(data => {
+    // Aquí puedes utilizar los datos obtenidos de la API o servidor
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+
+nuevoBotonAlerta.addEventListener("click", () => {
+  // Mostrar una nueva alerta utilizando SweetAlert2
+  Swal.fire({
+    icon: 'info',
+    title: '¡Nueva Alerta!',
+    text: 'Esta por realizar la compra de sus productos.'
+  });
+});
+
+const finalizarCompraBtn = document.getElementById("finalizar-compra");
+
+finalizarCompraBtn.addEventListener("click", finalizarCompra);
+
+function finalizarCompra() {
+  const productos = JSON.parse(localStorage.getItem("remeras"));
+
+  if (!productos || productos.length === 0) {
+    alert("El carrito está vacío. Agrega productos antes de finalizar la compra.");
+    return;
+  }
+
+  reiniciarCarrito();
+  revisarMensajeVacio();
+  alert("¡Compra exitosa! Gracias por tu pedido.");
+}
+
 document.getElementById("reiniciar").addEventListener("click", () => {
   contenedorTarjetas.innerHTML = "";
   reiniciarCarrito();
   revisarMensajeVacio();
 });
 
+
+
 /** Muestra o esconde el mensaje de que no hay nada en el carrito */
 function revisarMensajeVacio() {
   const productos = JSON.parse(localStorage.getItem("remeras"));
   carritoVacioElement.classList.toggle("escondido", productos);
   totalesContainer.classList.toggle("escondido", !productos);
+
+  
 }
